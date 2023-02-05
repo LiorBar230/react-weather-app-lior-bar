b5daf8a39beeb6df16d95bb987a4258168f66993
LiorBar230 <Liorbar230@gmail.com>
2023-02-04 21:33:39 -0500

    added basic css

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1 className="title">Weather App</h1>
         <Weather />
         <footer>
           This app was coded by{" "}
           <a href="https://www.linkedin.com/in/lior-bar-018186149/">
-            Lior Bar and is{" "}
+            Lior Bar{" "}
           </a>
+          and is{" "}
           <a href="https://github.com/LiorBar230/react-weather-app-lior-bar">
             open source on GitHub
           </a>
--- src/Weather.js
+++ src/Weather.js
@@ -5,21 +5,40 @@ export default function Weather() {
   return (
     <div className="container weather">
       <form>
-        <input type="search" placeholder="Enter city name" />
-        <input type="submit" value="search" />
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter city name"
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            {" "}
+            <input
+              type="submit"
+              value="search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
       </form>
       <h1>London</h1>
       <ul>
         <li>Saturday 20:06</li>
         <li>Cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-            alt="cloudy"
-          />{" "}
-          6°C
+          <div>
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+              alt="cloudy"
+            />
+            <span className="temperature">6</span>
+            <span className="unit">°C</span>
+          </div>
         </div>
         <div className="col-6">
           <ul>
