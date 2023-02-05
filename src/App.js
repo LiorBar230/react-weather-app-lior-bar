872e283fd16c0a19acbf011f42d85c0d2207a62a
LiorBar230 <Liorbar230@gmail.com>
2023-02-04 20:17:16 -0500

    connected weather and app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
+        <Weather />
         <footer>
           This app was coded by{" "}
           <a href="https://www.linkedin.com/in/lior-bar-018186149/">
