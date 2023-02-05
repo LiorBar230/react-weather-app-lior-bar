1953c682ede4cba5300e9b43760fe5c876b2f52c
LiorBar230 <Liorbar230@gmail.com>
2023-02-04 19:57:36 -0500

    created the framework + started adding basic html

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This app was coded by{" "}
+          <a href="https://www.linkedin.com/in/lior-bar-018186149/">
+            Lior Bar and is{" "}
+          </a>
+          <a href="">open source on GitHub</a>
+        </footer>
+      </div>
     </div>
   );
 }
