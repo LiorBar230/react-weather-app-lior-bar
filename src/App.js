7d9a43c1c361fd6fd4914274b70180167bd99ce7
LiorBar230 <Liorbar230@gmail.com>
2023-02-11 23:32:33 -0500

    cleaned up code + worked on forecast

--- src/App.js
+++ src/App.js
@@ -8,15 +8,27 @@ function App() {
         <Weather defaultCity="London" />
         <footer>
           This app was coded by{" "}
-          <a href="https://www.linkedin.com/in/lior-bar-018186149/">
+          <a
+            href="https://www.linkedin.com/in/lior-bar-018186149/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Lior Bar{" "}
           </a>
           and is{" "}
-          <a href="https://github.com/LiorBar230/react-weather-app-lior-bar">
+          <a
+            href="https://github.com/LiorBar230/react-weather-app-lior-bar"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             open source on GitHub
           </a>{" "}
           and
-          <a href="https://brilliant-wisp-f9cee6.netlify.app">
+          <a
+            href="https://brilliant-wisp-f9cee6.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             {" "}
             hosted on Netlify
           </a>
